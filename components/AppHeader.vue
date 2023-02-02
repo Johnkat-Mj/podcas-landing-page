15a808c4174c242a2c5bdd78426fe31d443fd86d
Johnk-Mj <johnkatembue4@gmail.com>
2023-02-02 10:29:28 +0100

    update[app header]

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,22 +1,29 @@
 <script lang="ts" setup>
-const navLinks=[
+import { Size, useScreen } from '@/composable/useScreen'
+
+const navIsOpen = useState('navIsOpen', () => false)
+const screen = useScreen()
+
+
+const navLinks = [
     {
-        text:"Podcast",
-        href:"/"
+        text: "Podcast",
+        href: "/"
     },
     {
-        text:"Solution",
-        href:"/"
+        text: "Solution",
+        href: "/"
     },
     {
-        text:"Blog",
-        href:"/"
+        text: "Blog",
+        href: "/"
     },
     {
-        text:"About",
-        href:"/"
+        text: "About",
+        href: "/"
     }
 ]
+
 </script>
 <template>
     <header class="absolute inset-x-0 top-0 py-3 z-50">
@@ -32,16 +39,66 @@ const navLinks=[
                         <span class="text-lg text-gray-700 dark:text-white">Podux</span>
                     </NuxtLink>
                 </div>
-                <div class="flex">
+                <div class="absolute invisible lg:visible lg:relative lg:flex">
                     <ul class="text-gray-700 dark:text-gray-100 flex items-center gap-8">
-                        <AtomsNavLink v-for="navItem in navLinks" :href="navItem.href" :text="navItem.text"/>
+                        <AtomsNavLink v-for="navItem in navLinks" :href="navItem.href" :text="navItem.text" />
                     </ul>
                 </div>
-                <div class="flex items-center bg-inherit gap-3 min-w-max">
-                    <ElementsThemeSwitcher/>
-                    <AtomsLinkBtn href="" variant="primary">
-                        Join Us
-                    </AtomsLinkBtn>
+
+                <div aria-hidden="true" class="hidden md:flex items-center bg-inherit gap-1 lg:gap-3 min-w-max">
+                    <ElementsThemeSwitcher />
+                    <div class="hidden lg:flex lg:items-center gap-4">
+                        <AtomsLinkBtn href="#" variant="secondary">
+                            Signin
+                        </AtomsLinkBtn>
+                        <AtomsLinkBtn href="#" variant="primary">
+                            Join Us
+                        </AtomsLinkBtn>
+                    </div>
+
+                    <div class="flex lg:hidden items-center">
+                        <ElementsDropDown>
+                            <template #trigger>
+                                <button
+                                    class="outline-none bg-transparent p-2.5 rounded-full text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                    </svg>
+                                </button>
+                            </template>
+                            <template #content>
+                                <ul class="flex flex-col">
+                                    <li
+                                        class="flex select-none cursor-pointer items-center gap-3 px-2 py-2 rounded-md transition hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-200">
+                                        <NuxtLink to="#">
+                                            Join-us
+                                        </NuxtLink>
+                                    </li>
+
+                                    <li
+                                        class="flex select-none cursor-pointer items-center gap-3 px-2 py-2 rounded-md transition hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-200">
+                                        <NuxtLink to="#">
+                                            Signin
+                                        </NuxtLink>
+                                    </li>
+                                </ul>
+                            </template>
+                        </ElementsDropDown>
+                    </div>
+
+                    <div class="flex lg:hidden border-l border-box-border pl-2">
+                        <button @click="navIsOpen = !navIsOpen" 
+                            class="outline-none w-7 h-auto flex flex-col relative">
+                            <span class="w-6 h-0.5 rounded-full bg-gray-500 dark:bg-gray-200 transition-all duration-300 ease-linear"
+                                :class="navIsOpen ? 'translate-y-1.5 rotate-[40deg] scale-x-100 ' : ' scale-x-50 origin-left'"></span>
+                            <span class="w-6 origin-center  mt-1 h-0.5 rounded-full bg-gray-500 dark:bg-gray-200 transition-all duration-300 ease-linear"
+                                :class="navIsOpen ? 'scale-x-0 opacity-0' : ''"></span>
+                            <span class="w-6 mt-1 h-0.5 rounded-full bg-gray-500 dark:bg-gray-200 transition-all duration-300 ease-linear"
+                                :class="navIsOpen ? '-translate-y-1.5 -rotate-[40deg] scale-x-100 ' : ' scale-x-75 origin-left'"></span>
+                        </button>
+                    </div>
                 </div>
             </nav>
         </AtomsContainer>
--- components/atoms/LinkBtn.vue
+++ components/atoms/LinkBtn.vue
@@ -15,6 +15,10 @@ if (variant === "primary") {
     spanClass = "border-2 border-primary"
     textCl = "text-primary"
 }
+else if(variant === "secondary"){
+    spanClass = "bg-primary/5"
+    textCl = "text-primary"
+}
 else if(variant === "costum"){
     spanClass=costumSpanClass
     textCl = costumTextColor
@@ -24,7 +28,7 @@ else if(variant === "costum"){
 <template>
     <NuxtLink :to="href" class="px-6 md:px-7 py-3 rounded-full relative group">
         <span v-if="variant!=='costum'" class="absolute inset-0 rounded-full group-hover:scale-105 origin-center transition-all ease-in-out" :class="spanClass"></span>
-        <span class="relative" :class="textCl">
+        <span class="relative flex items-center justify-center" :class="textCl">
             <slot/>
         </span>
     </NuxtLink>
